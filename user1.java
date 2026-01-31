025dfc92eb2f0c113f1386d20a254190e683513c
ShaikAyesha11218 <ayeshashaik11218@gmail.com>
2026-01-31 16:04:44 +0530

    Add files via upload

---
+++ user1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class user1 {
+     public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter age");
+        int age=sc.nextInt();
+        System.out.println("Enter a");
+        double a=sc.nextDouble();
+        System.out.println("Enter b");
+        float b=sc.nextFloat();
+        System.out.println("Enter str1");
+        String str1=sc.next();
+        System.out.println("Enter str2");
+        String str2=sc.nextLine();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(age);
+    }
+    
+}
