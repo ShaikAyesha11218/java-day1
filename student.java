eacce3b8d88756ff67e4b4513c5dfa22cefedca0
ShaikAyesha11218 <ayeshashaik11218@gmail.com>
2026-02-07 11:31:06 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,16 @@
+public class student {
+    public static void main(String[] args){
+        String s="ayesha";
+        int age=18;
+        char grade='a';
+        float mar=91.5f;
+        boolean st=true;
+        System.out.println(s);
+        System.out.println(age);
+        System.out.println(grade);
+        System.out.println(mar);
+        System.out.println(st);
+    }
+        
+    
+}
