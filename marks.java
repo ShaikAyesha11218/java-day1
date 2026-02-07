bcf0a0bdee9705ab08f8ae5de79f07a99a872b75
ShaikAyesha11218 <ayeshashaik11218@gmail.com>
2026-02-07 11:37:39 +0530

    Add files via upload

---
+++ marks.java
@@ -0,0 +1,13 @@
+public class marks {
+    public static void main(String[] args){
+        int total,avg;
+        int s1=95;
+        int s2=88;
+        int s3=89;
+        total=s1+s2+s3;
+        avg=s1+s2+s3/3;
+        System.out.println("Total="+total);
+        System.out.println("Avg="+avg);
+    }
+    
+}
