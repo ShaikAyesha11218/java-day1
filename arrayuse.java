628057fb2c23579109d283bbfb4e195aef8de11c
ShaikAyesha11218 <ayeshashaik11218@gmail.com>
2026-02-28 14:09:26 +0530

    Add files via upload

---
+++ arrayuse.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class arrayuse {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int arr[]=new int[5];
+        System.out.println("Enter the array elements:");
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+         System.out.println("array elements are:");
+        for(int j=0;j<arr.length;j++)
+        {
+            System.out.println(arr[j]);
+        }
+    }
+    
+}
